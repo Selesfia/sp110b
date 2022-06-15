f493c3e69180b356221d93b20804fd9666a32810
Selesfia <79678154+Selesfia@users.noreply.github.com>
2022-06-16 02:39:49 +0800

    Add files via upload

---
+++ Test.s
@@ -0,0 +1,28 @@
+	.file	"Test.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello World\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (GNU) 7.4.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
